d65d5c627230ff8d1eb928476bc7b3f74bbe9723
ygkim <ygkim527@naver.com>
2023-10-25 23:55:22 +0900

    예약 컴포넌트 작업 전 셋팅

--- src/app/components/listings/ListingClient.tsx
+++ src/app/components/listings/ListingClient.tsx
@@ -1,26 +1,102 @@
-import { useMemo } from 'react';
+import { useCallback, useMemo, useState, useEffect } from 'react';
+import { useRegisterModal } from '../hooks/useRegisterModal';
+import { useRouter } from 'next/navigation';
 import { SafeListing, SafeUser } from '@/app/types';
 import { Reservation } from '@prisma/client';
 import { categories } from '@/app/icons/categories';
 import Container from '../Container';
 import ListingHead from './ListingHead';
 import ListingInfo from './ListingInfo';
+import { eachDayOfInterval, differenceInCalendarDays } from 'date-fns';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+
+const initialDateRange = {
+  startDate: new Date(),
+  endDate: new Date(),
+  key: 'selection',
+};
 
 type ListingClientProps = {
   listing: SafeListing & {
     user: SafeUser;
   };
   currentUser: SafeUser | null;
-  reservations?: Reservation;
+  reservations?: Reservation[];
 };
 
 export default function ListingClient({
   listing,
   currentUser,
+  reservations = [],
 }: ListingClientProps) {
+  const router = useRouter();
+  const { onOpen } = useRegisterModal();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [totalPrice, setTotalPrice] = useState(listing.price);
+  const [dateRange, setDateRange] = useState(initialDateRange);
+
+  const onCreateReservation = useCallback(() => {
+    if (!currentUser) {
+      return onOpen();
+    }
+
+    setIsLoading(true);
+
+    axios
+      .post('/api/reservations', {
+        totalPrice,
+        startDate: dateRange.startDate,
+        endDateL: dateRange.endDate,
+        listingId: listing?.id,
+      })
+      .then(() => {
+        toast.success('Listing reserved!');
+        setDateRange(initialDateRange);
+        router.refresh();
+      })
+      .catch(() => {
+        toast.error('Something went wrong.');
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [totalPrice, dateRange, listing, router, currentUser, onOpen]);
+
+  const disabledDates = useMemo(() => {
+    let dates: Date[] = [];
+
+    reservations.forEach((reservation) => {
+      const range = eachDayOfInterval({
+        start: new Date(reservation.startDate),
+        end: new Date(reservation.endDate),
+      });
+
+      dates = [...dates, ...range];
+    });
+
+    return dates;
+  }, [reservations]);
+
   const category = useMemo(() => {
     return categories.find((item) => item.label === listing.category);
   }, [listing]);
+
+  useEffect(() => {
+    if (dateRange.startDate && dateRange.endDate) {
+      const dayCount = differenceInCalendarDays(
+        dateRange.endDate,
+        dateRange.startDate
+      );
+
+      if (dayCount && listing.price) {
+        setTotalPrice(dayCount * listing.price);
+      } else {
+        setTotalPrice(listing.price);
+      }
+    }
+  }, [dateRange, listing]);
 
   return (
     <Container>
@@ -43,6 +119,7 @@ export default function ListingClient({
               bathroomCount={listing.bathroomCount}
               locationValue={listing.locationValue}
             ></ListingInfo>
+            <div className='order-first mb-10 md:order-last md:col-span-3'></div>
           </div>
         </div>
       </div>
